b0427a8c757b1a7de240e41d0243771159f551d4
Cale Hoopes <caledh@gmail.com>
2023-11-12 09:40:09 -0800

    sdfgh

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -12,6 +12,45 @@ const api = express();
 const router = Router();
 router.get("hello", (req, res) => res.send("Hello"))
 
+
+function findTwoWordPhrases(text) {
+    const ignoreWords = ["want","a", "the", "in", "ve", "re", "an", "as", "can", "and", "is", "what", "need", "of", "with", "to", "who", "do", "it", "if", "you", "for", "i", "your", "not", "only", "been", "com", "dc"];
+    const words = text.match(/\b\w+\b/g).filter(word => !ignoreWords.includes(word.toLowerCase()) && word.length > 1);
+    if (!words || words.length < 2) return [];
+    const phrases = [];
+    for (let i = 0; i < words.length - 1; i++) {
+        const phrase = (words[i] + ' ' + words[i + 1]).toLowerCase(); // Convert to lowercase here
+        if (!/\d/.test(phrase)) { // Check for numbers in the phrase
+            phrases.push(phrase);
+        }
+    }
+    return phrases;
+}
+
+function countFrequencies(phrases) {
+    const frequencies = {};
+    for (const phrase of phrases) {
+        frequencies[phrase] = (frequencies[phrase] || 0) + 1;
+    }
+    return frequencies;
+}
+
+function displayPhrases(items) {
+    
+    // reduce to text blob
+    const text = items.map(i => {
+        (`${i.title} ${i.snippet}`)
+    }).join(' ');
+
+    // Find two-word phrases and count their frequencies
+    const phrases = findTwoWordPhrases(text);
+    const frequencies = countFrequencies(phrases);
+
+    // Sort phrases by frequency and take the top 30 results
+    const sortedPhrases = Object.entries(frequencies).sort((a, b) => b[1] - a[1]).slice(0, 30);
+    return sortedPhrases;
+}
+
 // title field, snippet field
 
 router.get("/gsearch", async (req, res) => {
@@ -19,8 +58,8 @@ router.get("/gsearch", async (req, res) => {
     const url = `https://customsearch.googleapis.com/customsearch/v1?key=${apiKey}&cx=${searchEngine}&q=${query}`
     const response = await fetch(url);
     const json = await response.json();
-    const titleSnippets = json.items.map(i => ({ title: i.title, snippet: i.snippet }));
-    res.send(titleSnippets);
+    const output = displayPhrases(json.items)
+    res.send(output);
 });
 
 api.use("/api/", router);
